3b2c307822984912461c5b5c4b34bbb7c3f3498c
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-06-20 00:19:06 +0800

    chore: update playground

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,30 +1,31 @@
-import { useState } from 'react';
-import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useRequest, useSize } from 'ahooks';
+import { useRef } from 'react';
+import { DirectedGraphVisualizer } from 'react-directed-graph-visualizer';
 
+// https://g6.antv.antgroup.com/examples/algorithm/case/#pattern-matching
 function App() {
-  const [count, setCount] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const containerSize = useSize(containerRef);
+
+  const { data, loading } = useRequest(() => fetch('/gaddi.json').then((res) => res.json()));
+
+  globalThis.console.log('[gaddi.json]', data);
+
+  // =========== render ===========
+  if (loading) {
+    return <h1 style={{ color: '#ae5ebb', fontSize: 32 }}>Loading...</h1>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div ref={containerRef} style={{ width: '100dvw', height: '100dvh' }}>
+      <DirectedGraphVisualizer
+        nodes={data.nodes ?? []}
+        edges={data.edges ?? []}
+        width={containerSize?.width}
+        height={containerSize?.height}
+        onSelectNode={(node) => console.log('Clicked node:', node)}
+      />
+    </div>
   );
 }
 
